16e94c4ae7184ca20bfe22a9fbb10e2c19ef66eb
Max Burri <max.burri@gmail.com>
2016-10-19 22:24:58 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# elm-quickstart
+
+Get up and running with Elm in minutes...
+
+
+## How?
+
+* clone this repo
+* run ```npm install```
+* run ```npm run dev```
+* open http://localhost:8000
+
+happy hacking!
---
+++ src/Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, h1)
+import Html.App as App
+import Html.Events exposing (onClick)
+
+
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { count : Int
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+update msg model =
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
+
+        Decrement ->
+            ( { model | count = model.count - 1 }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view model =
+    div []
+        [ h1 [] [ text "counters" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
